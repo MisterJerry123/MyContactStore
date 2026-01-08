a5207580eb6e73cde85917a7c91ef4745bbe9a19
MisterJerry123 <misterjerry12345@gmail.com>
2026-01-08 19:29:55 +0900

    feat: HomeViewModel 내 연락처 로드 로직 구현
    * `ContactRepository`를 주입받아 연락처 데이터를 가져올 수 있도록 `HomeViewModel` 생성자 수정
    * `MutableStateFlow`를 사용하여 연락처 목록 상태(`HomeState`) 관리 로직 추가
    * `viewModelScope`를 통해 비동기적으로 전체 연락처 데이터를 로드하는 `loadContacts` 함수 구현 및 초기화 시 호출하도록 설정

--- app/src/main/java/com/misterjerry/mycontactstore/presentation/home/HomeViewModel.kt
+++ app/src/main/java/com/misterjerry/mycontactstore/presentation/home/HomeViewModel.kt
@@ -1,6 +1,32 @@
 package com.misterjerry.mycontactstore.presentation.home
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.misterjerry.mycontactstore.data.repository.ContactRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
 
-class HomeViewModel: ViewModel() {
+class HomeViewModel(
+    private val repository: ContactRepository
+): ViewModel() {
+
+    private var _state = MutableStateFlow(HomeState())
+    val state = _state.asStateFlow()
+
+    init {
+        loadContacts()
+    }
+
+    fun loadContacts() {
+        viewModelScope.launch(Dispatchers.IO) {
+            val initData = repository.getAllContacts()
+            _state.value = _state.value.copy(contactList = initData)
+        }
+    }
+
+
+
+
 }
