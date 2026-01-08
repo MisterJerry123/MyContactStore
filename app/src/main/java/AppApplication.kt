bde0aa58746d5305f1bff19aaf4c7a111f3eec48
MisterJerry123 <misterjerry12345@gmail.com>
2026-01-08 19:28:44 +0900

    feat: Koin 의존성 주입 모듈 정의 및 설정
    * `ViewModelModule.kt`를 추가하여 `HomeViewModel`의 의존성 주입 설정
    * `RepositoryModule.kt`를 추가하여 `ContactRepository` 및 그 구현체인 `ContactRepositoryImpl`의 싱글톤 인스턴스 설정
    * `AppApplication.kt`에서 새롭게 정의한 `viewModelModule`과 `repositoryModule`을 Koin 모듈에 등록
    * `AppApplication` 클래스에 패키지 선언 추가 및 코드 포맷팅 수정

--- app/src/main/java/AppApplication.kt
+++ app/src/main/java/AppApplication.kt
@@ -1,16 +1,24 @@
+package com.misterjerry.mycontactstore
+
 import android.app.Application
 import com.misterjerry.mycontactstore.core.di.appModule
+import com.misterjerry.mycontactstore.core.di.repositoryModule
+import com.misterjerry.mycontactstore.core.di.viewModelModule
 import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
 
-class AppApplication: Application() {
+class AppApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         startKoin {
             androidLogger()
             androidContext(this@AppApplication)
-            modules(appModule)
+            modules(
+                appModule,
+                viewModelModule,
+                repositoryModule
+            )
         }
     }
 
